1c79e2963d24aaecb94da59cdd9e0958f124cfbb
Damien Pellerin <damienpellerin19@gmail.com>
2023-02-02 14:21:26 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ApiMovie from "./ApiMovie";
+import "./App.css";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
+import MovieSection from "./components/MovieSection";
 
 function App() {
+  const [moviesList, setMoviesList] = useState([]);
+  const [featuredData, setfeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAllMovies = async () => {
+      // Liste de tous les films
+      let list = await ApiMovie.getHomeMovies();
+      setMoviesList(list);
+
+      // Un seul film à l'affiche
+      let originals = list.filter((oneMovie) => oneMovie.slug === "top-rated");
+
+      let chooseRandomMovie = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[chooseRandomMovie];
+      let chosenInfo = await ApiMovie.getyMovieInfo(chosen.id, "movie");
+
+      setfeaturedData(chosenInfo);
+    };
+
+    loadAllMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header />
+      {featuredData && <FeaturedMovie films={featuredData} />}
+      <section className="lists">
+        {moviesList.map((item, key) => (
+          <MovieSection key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
